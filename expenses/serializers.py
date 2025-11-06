8a5027d88d3c2f1fd1c1d843b15baa13770def95
Mghosh2005 <2328102@kiit.ac.in>
2025-11-06 09:04:18 +0530

    Update serializers.py

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from .models import Transaction, MonthlyRollup
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+class MonthlyRollupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MonthlyRollup
+        fields = '__all__'
 
